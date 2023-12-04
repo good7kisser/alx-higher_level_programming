1a1565be965bccb6350060b213513a990f706359
good7kisser <ariiseverdean@gmail.com>
2023-12-04 10:40:30 -0800

    a

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     alx = ""
     for i in range(len(my_string)):
-        if (my_string[i] != 'c' and my_string[i] != 'c'):
+        if (my_string[i] != "c" and my_string[i] != "C"):
             alx += my_string[i]
     return alx
