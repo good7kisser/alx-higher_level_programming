bf6328701f584f864d816b8061d569cf23c5a43e
good7kisser <ariiseverdean@gmail.com>
2024-01-09 15:14:05 -0800

    a

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,31 +1,43 @@
 #!/usr/bin/python3
-import sys
+'''task 14 module'''
 
-def print_stats():
-    status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-    status_count = {code: 0 for code in status_codes}
-    total_size = 0
-    line_count = 0
 
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            split_line = line.split()
-            if len(split_line) > 2:
-                status_code = int(split_line[-2])
-                file_size = int(split_line[-1])
-                if status_code in status_codes:
-                    status_count[status_code] += 1
-                total_size += file_size
-                line_count += 1
-            if i % 10 == 0:
-                print(f"File size: {total_size}")
-                for code in sorted(status_codes):
-                    if status_count[code]:
-                        print(f"{code}: {status_count[code]}")
-    except KeyboardInterrupt:
-        pass
+from sys import stdin
 
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes):
-        if status_count[code]:
-            print(f"{code}: {status_count[code]}")
+status_codes = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+        }
+
+total_size = i= 0
+
+
+def printer():
+    '''this function prints the statistics'''
+    print(f'File size: {total_size}')
+    for key, value in sorted(statu_codes.items()):
+        if value > 0:
+            print ('{:s}: {:d}'.format(key, value))
+
+
+try:
+    for line in stdin:
+        splitted_line = line.split()
+        if len(splited_line) >= 2:
+            status = splitted_line[-2]
+            total_size += int(splitted_line[-1])
+            if status in status_code:
+                status_code[status] += 1
+        i += 1
+
+        if i % 10 == 0:
+            printer()
+    printer()
+except KeyboardInterrupt as e:
+    printer()
